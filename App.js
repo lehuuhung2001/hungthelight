e0b095ec73119ca0e2af5fc4d4e70d9c4219a9e4
Le Huu Hung <huuhung1962001@gmail.com>
2022-01-05 14:49:45 +0700

    change

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image , Dimensions, Switch } from 'react-native';
+import bulboff from './assets/c02n41k51co3o9rj1edg.jpg';
+import bulbopen from './assets/c02n43451co3o9rj1ee0.jpg';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style = {styles.editimg} source = {isEnabled ? bulbopen : bulboff}/>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
@@ -17,4 +28,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  editimg: {
+    width : windowWidth,
+    height: windowHeight - 100,
+    resizeMode: 'contain',
+    backgroundColor:'black',
+  }
 });
